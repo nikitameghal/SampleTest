4d8126aea6d6efa2eed2c6bf084f14b3a76eda71
nikitameghal <niki.1.meghal@gmail.com>
2022-08-18 17:22:52 +0530

    hello

--- com.Sample_test/src/test/java/com/SampleTest_Prac/SampleTest.java
+++ com.Sample_test/src/test/java/com/SampleTest_Prac/SampleTest.java
@@ -26,6 +26,7 @@ WebDriver driver=new ChromeDriver();
 		System.out.println("-----------------");
 		System.out.println("welcome to jenkins");
 		System.out.println("-----------------");
+		System.out.println("welcome to jenkins");
 		
 		driver.close();
 	}
